dc2b4060da9811b7cd48f3ce1199356917ab455f
Exhausted <66200524+3xhaust3dAF@users.noreply.github.com>
2024-10-23 18:10:33 +0300

    Create 3a-t4.3.cpp
    Task 4 but with check for variable type

---
+++ 3a-t4.3.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream> 
+using namespace std;
+
+int main()
+{
+	int k, i = 1, sum_k = 0, number;
+	cout << " Enter the amount of integers to sum = "; cin >> k;
+	if (k == 0)
+	{
+		cout << "The number of integers is 0";
+	}
+	else
+	{
+		do {
+			cout << "Enter integer nr. " << i << ": ";
+			cin >> number;
+			while (cin.fail())
+			{
+				cin.clear();
+				cin.ignore();
+				cout << " Invalid input! Enter the integer nr. " << i << ": "; cin >> number;
+			}
+			sum_k += number; i++;
+		} while (i <= k);
+		cout << " The total sum of " << k << " integers is: " << sum_k;
+	}
+	return 0;
+}
